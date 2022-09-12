1a1c64480d5d64ec5f0d6ea63f48fc7fc834bfd8
adamulrich <adamulrich@hotmail.com>
2022-09-11 22:27:37 -0700

    final check

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+function setDateValues() {
+    date = new Date().getFullYear();
+    document.querySelector("#year").innerHTML = date
+    document.querySelector("#timestamp").innerHTML= document.lastModified;
+}
