bc5dcc1b48c1c6c58a1a1a12fdd4175d07c715fc
Adam Ulrich <adamulrich@hotmail.com>
2022-10-04 23:35:34 -0700

    adding personas and stubbing out week 4.

---
+++ chamber/scripts/script.js
@@ -0,0 +1,12 @@
+document.querySelector("#date").innerHTML = new Date().toDateString();
+
+// function foo(j) {
+//     for (let i = j; i >= 0 ; i--) {
+//         console.log(`begin: ${i}`);
+//       }
+//     for (let i = 0; i <= j ; i++) {
+//         console.log(`end: ${i}`);
+//       }
+// }
+
+// foo(10);
