cffe0277c6c1995f73ae67e9bf47c2222fab55f4
adamulrich <adamulrich@hotmail.com>
2022-10-07 01:06:14 -0700

    layout mess

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,12 +1,23 @@
-document.querySelector("#date").innerHTML = new Date().toDateString();
+date = new Date().getFullYear();
+document.querySelector("#year").textContent = date;
+document.querySelector("#timestamp").textContent= document.lastModified;
 
-// function foo(j) {
-//     for (let i = j; i >= 0 ; i--) {
-//         console.log(`begin: ${i}`);
-//       }
-//     for (let i = 0; i <= j ; i++) {
-//         console.log(`end: ${i}`);
-//       }
-// }
+// const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(new Date());
+// document.querySelector("#date").innerHTML = fullDate;
 
-// foo(10);
+// const hamBtn = document.querySelector('#hamBtn');
+// const mainNav = document.querySelector('#mainNav')
+
+// hamBtn.addEventListener('click', () => {mainNav.classList.toggle('open')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainNav.classList.remove('open')};
+
+function toggleMenu() {
+    document.getElementById("mainNav").classList.toggle("open");
+    document.getElementById("hamBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamBtn");
+x.onclick = toggleMenu;
+
