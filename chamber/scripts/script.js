85908dc36aca02b4e5706f6b9fc7def78c2eb2c4
adamulrich <adamulrich@hotmail.com>
2022-10-22 22:08:07 -0700

    week 7 initial build.

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -23,3 +23,46 @@ function toggleMenu() {
 
 // add event to toggle 
 document.getElementById("ham_btn").onclick = toggleMenu;
+
+
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    } else {
+        img.src = src;
+        img.removeAttribute("data-src");
+    }
+}
+
+
+const loadImage = image => {
+    image.classList.add('fade-in');
+    image.src = image.dataset.src;
+    image.srcset = image.dataset.srcset;
+  }
+
+const imgOptions = {
+	root: document.querySelector('#scrollArea'),
+	rootMargin: '10px',
+	threshold: 1.0
+};
+
+const imgObserver = new IntersectionObserver((entries, 
+    imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+})
+
