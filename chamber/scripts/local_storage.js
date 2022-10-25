988997783bfd21cc25e9292407feed643a538759
adamulrich <adamulrich@hotmail.com>
2022-10-24 17:22:03 -0700

    adding dot notation for local storage.

--- chamber/scripts/local_storage.js
+++ chamber/scripts/local_storage.js
@@ -3,18 +3,18 @@ const visitsDisplay = document.getElementById("visits");
 const daysSinceVisit = document.getElementById("days-since-last-visit");
 
 // get the stored value in localStorage
-let numVisits = localStorage["numVisits"]
+let numVisits = localStorage.numVisits
 
 // determine if this is the first visit or display the number of visits.
 if (numVisits != undefined) {
 	visitsDisplay.textContent = numVisits;
     numVisits ++;
-    localStorage["numVisits"] = numVisits;
+    localStorage.numVisits = numVisits;
     visitsDisplay.innerText = numVisits;
 
 } else {
 	visitsDisplay.textContent = '0';
-    localStorage["numVisits"] = 1;
+    localStorage.numVisits = 1;
 }
 
 // increment the number of visits.
@@ -22,14 +22,14 @@ if (numVisits != undefined) {
 let date = new Date();
 
 // if the date isn't in storage, put it there
-if (localStorage["lastVisitDate"] == undefined) {
-    localStorage["lastVisitDate"] = '' + date.getTime();  
+if (localStorage.lastVisitDate == undefined) {
+    localStorage.lastVisitDate = '' + date.getTime();  
     daysSinceVisit.innerText = "0"
 } else {
     // get the storage and calculate
-    let storageDate = new Date(parseInt(localStorage["lastVisitDate"], 10));
+    let storageDate = new Date(parseInt(localStorage.lastVisitDate, 10));
     let daysSinceLastVisit = ((date - storageDate)/86400000).toString().substring(1,8);
     daysSinceVisit.innerText = daysSinceLastVisit;
-    localStorage["lastVisitDate"] = '' + date.getTime();  
+    localStorage.lastVisitDate = '' + date.getTime();  
     
 }
